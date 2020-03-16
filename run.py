86db2b718cd343edc5aa3dae3192600ccc39c7f9
Eric Jiang <43181808+e5jiang@users.noreply.github.com>
2020-03-16 16:02:04 -0700

    update run.py

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ from etl import *
 
 if __name__ == "__main__":
     args = sys.argv
-    env = json.load(open('config/env.json'))
+    env = json.load(open('../config/env.json'))
     
     os.chdir(args[1])
 
-    data_params = json.load(open('config/data-params.json'))
+    data_params = json.load(open('../config/data-params.json'))
 
     chmod_files()
 
