5838237d33a0fc19b79acc28e3ee556d1f733610
Eric Jiang <43181808+e5jiang@users.noreply.github.com>
2020-03-16 23:03:55 -0700

    update run.py

--- run.py
+++ run.py
@@ -19,12 +19,12 @@ if __name__ == "__main__":
     
     plot_pca()
 
-    with open("config/env.json", "r") as f:
+    with open("../config/env.json", "r") as f:
         env = json.load(f)
 
     env['output-paths'] = ['test-project/outputs/SP1.vcf.gz', 'test-project/outputs/pca.png']
 
-    with open("config/env.json", "w") as f:
+    with open("../config/env.json", "w") as f:
         json.dump(env, f)
 
 
